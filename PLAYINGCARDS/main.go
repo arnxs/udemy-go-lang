98e128fe360d9acbad24623a97eb2f12dcf711d1
onjo <onjo@shaw.ca>
2018-11-13 21:37:49 -0700

    touch up TestNewDeck()

--- PLAYINGCARDS/main.go
+++ PLAYINGCARDS/main.go
@@ -3,7 +3,7 @@ package main
 func main() {
 	cards := newDeck()
 	// hand, remainingCards := cards.deal(5)
-	cards.shuffle()
+	// cards.shuffle()
 	cards.saveToFile("my-cards")
 
 	cards = loadFromFile("my-cards")
