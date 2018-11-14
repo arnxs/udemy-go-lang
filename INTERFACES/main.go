e3b74e4e591f3b9988907ea49ce8cdad5b9a3777
onjo <onjo@shaw.ca>
2018-11-14 00:03:14 -0700

    play around with interfaces

--- INTERFACES/main.go
+++ INTERFACES/main.go
@@ -2,15 +2,28 @@ package main
 
 import "fmt"
 
+type bot interface {
+	getGreeting() string
+	sayFarewell() string
+}
+
 type spanishBot struct{}
 type englishBot struct{}
+type germanBot struct{}
 
 func main() {
 	eb := englishBot{}
 	sb := spanishBot{}
+	gb := germanBot{}
 
 	printGreeting(eb)
-	printGreetingSpanish(sb)
+	printGreeting(sb)
+	printGreeting(gb)
+
+	printFarewell(eb)
+	printFarewell(sb)
+	printFarewell(gb)
+	// printGreetingSpanish(sb)
 }
 
 func (sb spanishBot) getGreeting() string {
@@ -21,10 +34,30 @@ func (eb englishBot) getGreeting() string {
 	return "Hello"
 }
 
-func printGreeting(eb englishBot) {
-	fmt.Println(eb.getGreeting())
+func (gb germanBot) getGreeting() string {
+	return "German Hello"
 }
 
-func printGreetingSpanish(sb spanishBot) {
-	fmt.Println(sb.getGreeting())
+func (sb spanishBot) sayFarewell() string {
+	return "sp bye"
 }
+
+func (eb englishBot) sayFarewell() string {
+	return "eb bye"
+}
+
+func (gb germanBot) sayFarewell() string {
+	return "gb bye"
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+func printFarewell(b bot) {
+	fmt.Println(b.sayFarewell())
+}
+
+// func printGreetingSpanish(sb spanishBot) {
+// 	fmt.Println(sb.getGreeting())
+// }
