4eb2574f94ea7a2ee26a3d023e2eeab5f80c981b
onjo <onjo@shaw.ca>
2018-11-14 00:37:07 -0700

    explore Reader Read() interface

--- HTTP/main.go
+++ HTTP/main.go
@@ -7,11 +7,23 @@ import (
 )
 
 func main() {
-	resp, err := http.Get("http://google.ca")
+	// url := "https://medium.com/@avery_1242/my-experience-with-google-foobar-tips-for-tackling-googles-legendary-coding-challenge-dbc20a054e4e"
+	url := "http://google.com"
+	resp, err := http.Get(url)
 	if err != nil {
 		fmt.Println("Error: ", err)
 		os.Exit(1)
 	}
 
-	fmt.Println(resp)
+	// fmt.Println(resp.Status)
+	// fmt.Println(resp.StatusCode)
+	// bs := []byte{}
+	bs := make([]byte, 99999)
+	size, err := resp.Body.Read(bs)
+	// if err != nil {
+	// 	fmt.Println("Error: ", err)
+	// 	os.Exit(1)
+	// }
+	fmt.Println("response:", size, string(bs))
+	fmt.Println(resp.ContentLength)
 }
