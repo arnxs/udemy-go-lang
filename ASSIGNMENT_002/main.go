94ffcf28f3af25071d1667feba0edb2b00c76be8
onjo <onjo@shaw.ca>
2018-11-15 00:19:58 -0700

    improve assignment 2

--- ASSIGNMENT_002/main.go
+++ ASSIGNMENT_002/main.go
@@ -1,29 +1,23 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"os"
 )
-
-type fileReader struct {
-	contents []byte
-}
 
 func main() {
 	if len(os.Args) > 1 {
 		fmt.Println("reading this file...", os.Args[1])
 		filename := os.Args[1]
-		bs, err := ioutil.ReadFile(filename)
+		// bs, err := ioutil.ReadFile(filename)
+		r, err := os.Open(filename)
 		if err != nil {
 			fmt.Println("Error:", err)
 		} else {
 			// printByteSlice(bs)
 			// fr := fileReader{contents: bs}
-			fr := bytes.NewReader(bs)
-			io.Copy(os.Stdout, fr)
+			io.Copy(os.Stdout, r)
 		}
 
 	} else {
@@ -31,11 +25,3 @@ func main() {
 		os.Exit(1)
 	}
 }
-
-// func (fr fileReader) Read(p []byte) (n int, err error) {
-// 	// fmt.Println(string(fr.contents))
-// 	// copy(p, fr.contents)
-
-// 	return len(fr.contents), nil
-// 	// return 1, nil
-// }
