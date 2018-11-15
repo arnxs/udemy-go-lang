1775b96d5e3e24bde746c1f11d95dfaeeab73187
onjo <onjo@shaw.ca>
2018-11-15 00:23:05 -0700

    final solution for assignment 2

--- ASSIGNMENT_002/main.go
+++ ASSIGNMENT_002/main.go
@@ -8,18 +8,13 @@ import (
 
 func main() {
 	if len(os.Args) > 1 {
-		fmt.Println("reading this file...", os.Args[1])
 		filename := os.Args[1]
-		// bs, err := ioutil.ReadFile(filename)
 		r, err := os.Open(filename)
 		if err != nil {
 			fmt.Println("Error:", err)
 		} else {
-			// printByteSlice(bs)
-			// fr := fileReader{contents: bs}
 			io.Copy(os.Stdout, r)
 		}
-
 	} else {
 		fmt.Println("Not enough arguments.  Please pass filename to read")
 		os.Exit(1)
